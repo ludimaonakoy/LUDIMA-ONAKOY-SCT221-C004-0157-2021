42954cfdaa4a1d8eaf54551216b33a456f43f6de
unknown <ludimaonakoy.ludimajunior@gmail.com>
2023-11-27 12:16:29 +0300

    A function that prints a sting, followed by a new line

---
+++ QUESTION-FOUR-PRINTASTRING.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        PrintWithNewLine("Hello, World!");
+    }
+
+    static void PrintWithNewLine(string message)
+    {
+        // Print the string followed by a new line
+        Console.WriteLine(message);
+    }
+}
+
+//NAME: LUDIMA ONAKOY JUNIOR
+//REG-NUMBER: SCT221-C004-0157/2021
+//CLASS : 3.2 EVENING JKUAT-CBD.
